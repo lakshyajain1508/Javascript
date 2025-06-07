9a7ef46aa3613fb1c3e74c69e7eec622703845e7
Lakshya Jain <lakshyajain1508@gmail.com>
2025-06-07 20:36:19 +0530

    update Arrays and small projects

--- array/js/arr.js
+++
@@ -1,8 +0,0 @@
-let fruits = ['1','2' ,'3','4'];
-    document.write("Number List : - ");
-
-    for (let index = 0; index < fruits.length; index++) {
-        let element = fruits[index];
-
-        document.write("<li class='hn'>" + element + "</li>");
-    }
